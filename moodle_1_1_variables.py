5be877a8e11160c0bde35801b7778176ebf7e43a
ArtDoor02 <irealtema@gmail.com>
2025-01-10 18:27:18 +0300

    Add files via upload

---
+++ moodle_1_1_variables.py
@@ -0,0 +1,6 @@
+Executed_Homeworks=12
+Spent_hours=1.5
+CurseName="Python"
+AverageTime=Spent_hours/Executed_Homeworks
+
+print(f"""Курс {CurseName}, всего задач: {Executed_Homeworks}, затрачено часов: {Spent_hours}, среднее время выполнения: {AverageTime} часа.""")
