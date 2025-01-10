09c1962e572e648f6c2f94a9e0dc2ef67cb4e4f7
ArtDoor02 <irealtema@gmail.com>
2025-01-10 18:34:58 +0300

    Update and rename moodle_1_1_variables.py to moodle_1_4_variables.py

--- moodle_1_1_variables.py
+++
@@ -1,6 +0,0 @@
-Executed_Homeworks=12
-Spent_hours=1.5
-CurseName="Python"
-AverageTime=Spent_hours/Executed_Homeworks
-
-print(f"""Курс {CurseName}, всего задач: {Executed_Homeworks}, затрачено часов: {Spent_hours}, среднее время выполнения: {AverageTime} часа.""")
