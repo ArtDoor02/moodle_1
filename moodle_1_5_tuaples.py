4df77ecc5201f87d8e3deae3358b070faff419af
ArtDoor02 <irealtema@gmail.com>
2025-01-10 21:21:01 +0300

    Update moodle_1_5_tuaples.py

--- moodle_1_5_tuaples.py
+++ moodle_1_5_tuaples.py
@@ -1,9 +1,13 @@
 import random
-immutable_var=(1,2,3.46,True,False,"Artem")
+immutable_var=(1,2,3,4,5,3.46,True,False,"Artem")
 print(immutable_var)
 #immutable_var[0]=5 неккоректно, так как тип неизменяемый по аналогии с перечислениями enum из c++
 mutable_list=[1,2,3,4,5]
 print(mutable_list)
 for i in range(len(mutable_list)):
   mutable_list[i]=random.randint(1,100)
+print(mutable_list)
+mutable_list.append(immutable_var[0])
+print(mutable_list)
+mutable_list.extend(immutable_var[0:8])
 print(mutable_list)
